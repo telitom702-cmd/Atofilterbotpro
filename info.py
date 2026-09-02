cb937a219460eb23503a33418e7228b9eb74af5c
telitom702-cmd <telitom702@gmail.com>
2026-09-02 11:10:11 +0600

    Update info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'royal_search')   # Session name for the bot
 API_ID = int(environ.get('API_ID', '')) # API ID from my.telegram.org
 API_HASH = environ.get('API_HASH', '')  # API Hash from my.telegram.org
-BOT_TOKEN = environ.get('BOT_TOKEN', "")    # Bot token from @BotFather
+BOT_TOKEN = environ.get('BOT_TOKEN', "8748178338:AAFkn8pm-ATKP9D8TOJaVKNwhj6LdzzBaKg")    # Bot token from @BotFather
 
 # ============================
 # Bot Settings Configuration
