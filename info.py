482b04ba5b5572ba9c34fd2d34e61c05a817abc8
telitom702-cmd <telitom702@gmail.com>
2026-09-04 18:24:46 +0600

    Update info.py

--- info.py
+++ info.py
@@ -122,7 +122,7 @@ THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "54000"))
 # ============================
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/request_gruop') # Group link for the bot
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/request_gruop') # Owner link for the bot
-UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/ip_update') # Update channel link for the bot
+UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/jacpotfilmm') # Update channel link for the bot
 
 # ============================
 # User Configuration
