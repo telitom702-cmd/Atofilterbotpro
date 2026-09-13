ad5168728b399913caea57ecfaafa65a0ce46c32
telitom702-cmd <telitom702@gmail.com>
2026-09-13 23:52:56 +0600

    Update thumbnail.py

--- plugins/thumbnail.py
+++ plugins/thumbnail.py
@@ -12,7 +12,6 @@ from pyrogram.types import (
 from pyrogram.errors import MessageNotModified
 
 # আপনার প্রজেক্ট অনুযায়ী এগুলো ঠিক করুন
-from info import Config
 from database import db
 
 logger = logging.getLogger(__name__)
